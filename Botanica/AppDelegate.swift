262b895ce5d7238dc0b22636ce4bb40c0b634750
SimNay <snayyar@ucsd.edu>
2021-05-23 00:51:22 -0700

    test comment

--- Botanica/AppDelegate.swift
+++ Botanica/AppDelegate.swift
@@ -12,7 +12,8 @@ import Parse
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    //test comment
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let parseConfig = ParseClientConfiguration {

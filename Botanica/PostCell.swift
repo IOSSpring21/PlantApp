16c4bf6d9ffaa8249bdb5122494a380f3dddf1a6
SimNay <snayyar@ucsd.edu>
2021-05-23 12:32:24 -0700

    feed without image
    - need to resolve image column

---
+++ Botanica/PostCell.swift
@@ -0,0 +1,31 @@
+//
+//  PostCell.swift
+//  Botanica
+//
+//  Created by SIMRAN on 5/23/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

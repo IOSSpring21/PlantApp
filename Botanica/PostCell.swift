7bc77665d707fe348245c435b5c5b950d2eb0948
Szak123 <sampersonal569@gmail.com>
2021-05-31 17:50:33 -0700

    Profile/Settings fix

--- Botanica/PostCell.swift
+++ Botanica/PostCell.swift
@@ -10,10 +10,7 @@ import UIKit
 class PostCell: UITableViewCell {
 
     @IBOutlet weak var photoView: UIImageView!
-    
     @IBOutlet weak var usernameLabel: UILabel!
-    
-    
     @IBOutlet weak var captionLabel: UILabel!
     
     
--- Botanica/PostViewController.swift
+++ Botanica/PostViewController.swift
@@ -28,8 +28,6 @@ class PostViewController: UIViewController {
             
         }
         
-        
-        
     }
     
     

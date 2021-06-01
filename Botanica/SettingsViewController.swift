586704f6088a653d3f3cacdf06e9b027d4ff3462
Szak123 <sampersonal569@gmail.com>
2021-06-01 13:07:06 -0700

    Fixed Back Control/submit button

---
+++ Botanica/SettingsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsViewController.swift
+//  Botanica
+//
+//  Created by Sam Zakeri on 6/1/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
